bcbd869f236ae16c57613566e5b8bd4ae21bcb2b
Cung Bui Tuan Anh <cungbuituananh@gmail.com>
2021-02-04 00:46:02 +0700

    Update script.js

--- TetHoliday/script.js
+++ TetHoliday/script.js
@@ -29,7 +29,7 @@ filterSelection("all")
 function filterSelection(c) {
   var x, i; 
   x = document.getElementsByClassName("item"); 
-  if ( c == "all") c == ""; 
+  if ( c == "all") c = ""; 
   for ( i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
