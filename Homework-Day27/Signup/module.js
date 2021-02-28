6986191d5de6b63a9b70b82550762c41bbe7d4f0
Cung Bui Tuan Anh <cungbuituananh@gmail.com>
2021-02-28 16:26:43 +0700

    Create module.js

---
+++ Homework-Day27/Signup/module.js
@@ -0,0 +1,30 @@
+// B1 Get elements 
+
+const nickname = document.getElementById("nickname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
+
+var messengerInvalid = document.getElementById("messenger-invalid")
+export function nickValidation() {
+    let nickContent = nickname.value.trim();
+    if (nickContent == "") {
+        messengerInvalid.innerHTML = "Tên đăng nhập chứa A-Z a-z 0-9 & các ký tự khác"
+        return false;
+    } else if (nickContent.length < 8) {
+        messengerInvalid.innerHTML = "Tên đăng nhập có nhiều hơn 8 ký tự"
+        return false;
+    } 
+    return true;
+}
+
+export function nickValid() {
+    nickname.classList.add("is-valid");
+    nickname.classList.remove("is-invalid");
+
+}
+export function nickInvalid() {
+    nickname.classList.add("is-invalid");
+    nickname.classList.remove("is-valid");
+   
+}
