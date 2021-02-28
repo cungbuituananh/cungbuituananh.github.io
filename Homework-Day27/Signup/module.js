5292a0fcc57e29d2bc64678e73cb95907c6d09a4
Cung Bui Tuan Anh <cungbuituananh@gmail.com>
2021-02-28 22:59:58 +0700

    Update module.js

--- Homework-Day27/Signup/module.js
+++ Homework-Day27/Signup/module.js
@@ -5,18 +5,20 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("password-confirm");
 
-// Lấy element messenger 
-var messengerInvalid = document.getElementById("messenger-invalid");
+// Tạo biến lấy element messenger 
+var messengerInvalid = document.getElementById("nickname-invalid");
+var emailInvalid = document.getElementById("email-invalid");
+var passwordInvalid = document.getElementById("password-invalid");
+var passwordConfirmInvalid = document.getElementById("passwordConfirm-invalid");
 
 
+//  Điều kiện check nickname
 export function nickValidation() {
   let nickContent = nickname.value.trim();
-  if (nickContent == "") {
+  // Điều kiện khi nội dung nhập là mảng rỗng
+  if (nickContent == "" || nickContent.length < 8 ) {
     messengerInvalid.innerHTML =
-      "Tên đăng nhập chứa A-Z a-z 0-9 & các ký tự khác";
-    return false;
-  } else if (nickContent.length < 8) {
-    messengerInvalid.innerHTML = "Tên đăng nhập có nhiều hơn 8 ký tự";
+      "Tên đăng nhập có đô dài trên 8 ký tự bao gồm A-Z & 0-9 ";
     return false;
   }
   return true;
@@ -29,4 +31,52 @@ export function nickValid() {
 export function nickInvalid() {
   nickname.classList.add("is-invalid");
   nickname.classList.remove("is-valid");
+}
+
+// Điều kiện check password
+export function passwordValidation() {
+    let passwordContent = password.value.trim();
+
+    if (passwordContent == "") {
+        passwordInvalid.innerHTML = "Vui lòng nhập mật khẩu";
+        return false;
+    } else if (!isNaN(passwordContent) || passwordContent.length < 6) {
+        passwordInvalid.innerHTML = "Mật khẩu phải có ít nhất 8 ký tự bao gồm A-Z & 0-9";
+        return false;
+    }
+
+    return true;
+}
+
+export function passValid() {
+    password.classList.add("is-valid");
+    password.classList.remove("is-invalid");
+}
+export function passInvalid() {
+    password.classList.add("is-invalid");
+    password.classList.remove("is-valid");
+}
+
+
+// Điều kiện check password confirm 
+export function passwordConfirmValidation() {
+    let passwordConfirmContent = passwordConfirm.value.trim();
+    let passwordContent = password.value.trim();
+
+    if ( passwordConfirmContent == "") {
+        passwordConfirmInvalid.innerHTML = "Vui lòng nhập lại mật khẩu";
+        return false;
+    } else if ( passwordConfirmContent == passwordContent)   {
+        return true;
+    }
+}
+
+export function passConfirmValid() {
+    passwordConfirm.classList.add("is-valid");
+    passwordConfirm.classList.remove("is-invalid")
+}
+
+export function passConfirmInvalid() {
+    passwordConfirm.classList.add("is-invalid");
+    passwordConfirm.classList.remove("is-valid")
 }
