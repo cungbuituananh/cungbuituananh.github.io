e71bb62cb50e02b4004cfc02bbc49fdf772c870d
Cung Bui Tuan Anh <cungbuituananh@gmail.com>
2021-03-09 20:17:18 +0700

    Update ulti.js

--- Homework-Day29/js/ulti.js
+++ Homework-Day29/js/ulti.js
@@ -1,56 +1,41 @@
-// Export chức năng get / post
-
 export default function (options) {
-  // Sử dụng Destructuring
-  const { method, url, condition } = options;
-
-  // Return lại 1 promise
+  const {method, url, body} = options;
+  
   return new Promise((resolve, reject) => {
-    // Tạo một đối tượng xhr mới
     let xhr = new XMLHttpRequest();
-
-    // xác định định dạng trả về là json
     xhr.responseType = "json";
-
-    // xác định yêu cầu phương thức
-    xhr.open(method, url);
-
-    // gửi yêu cầu đến server khi điều kiện là true / false
-    xhr.send(condition ? condition : null);
-
-    // tình trạng của request khi đc tải thành công hoặc thất bại
+    xhr.open(method,url);
+    xhr.send(body ? body : null);
     xhr.onload = () => {
       if (xhr.status == 200) {
-        resolve(xhr.response);
+        resolve(xhr.response)
       } else {
         reject(xhr.status + ":" + xhr.statusText);
       }
     };
-
-    xhr.onerror = () => reject("Có lỗi xảy ra");
+    xhr.onerror = () => reject("Ooops!");
   });
 }
 
-
 export function getUser(users,id) {
-    for(let i = 0; i < users.length; i++ ) {
-        if(users[i].id == id) {
-            return users[i];
-        }
+  for (let i = 0; i < users.length; i++) {
+    if ( users[i].id == id ) {
+      return users[i];
     }
+  }
 }
 
 export function createPost(post, user) {
   let html = `
-        <div class="post">
+        <div class="post col-lg-6">
             <a class="post-link" href="post.html?id=${post.id}">
                 <h3 class="post-title">${post.title}</h3>
             </a>
 
             <p class="post-short-desciption">${post.body}</p>
-            <p class="post-author">By
+            <p class="post-author">Author
                 <a class="author-link" href="postbyuser.html?userId=${user.id}">
-                    ${user.name}
+                    <i>${user.name}</i>
                 </a>
             </p>
         </div>
@@ -60,24 +45,22 @@ export function createPost(post, user) {
 }
 
 export function createPostDetail(post) {
-    let html = `
-          <div class="post">
-              <h2 class="post-title">${post.title}</h2>
-              <p class="post-desciption">${post.body}</p>
-          </div>
-      `;
-  
-    return html;
-  }
+  let html = `
+        <div class="post">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-desciption">${post.body}</p>
+        </div>
+    `;
 
-
+  return html;
+}
 export function createComment(comment) {
-    let html = `
-          <div class="comment">
-              <h4 class="comment-name">${comment.name}</h4>
-              <p class="comment-email">${comment.email}</p>
-              <p class="comment-body">${comment.body}</p>
-          </div>
-      `;
-    return html;
-  }
+  let html = `
+        <div class="comment">
+            <h4 class="comment-name">${comment.name}</h4>
+            <p class="comment-email">${comment.email}</p>
+            <p class="comment-body">${comment.body}</p>
+        </div>
+    `;
+  return html;
+}
