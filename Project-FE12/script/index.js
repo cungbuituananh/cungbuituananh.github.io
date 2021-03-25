36e35f0d3ec594f7185b50705a0f77280e30d5c1
Cung Bui Tuan Anh <cungbuituananh@gmail.com>
2021-03-25 08:10:08 +0700

    push

--- Project-FE12/script/index.js
+++ Project-FE12/script/index.js
@@ -1,86 +1,100 @@
-let btnLogin = document.getElementById("btn-login");
-let btnSignup = document.getElementById("btn-singup");
-let btnLogout = document.getElementById("btn-logout");
-let username = document.getElementById("username");
-// toàn bộ div chưa form login
-let ctnLoginForm = document.getElementById("ctn-loginForm");
-// Toàn bộ điv chứa form signup
-let ctnSignupForm = document.getElementById("ctn-signupForm");
-// form login
-let formSignup = document.getElementById("form-signup");
-// event khi click click nut dang nhap
-let displayLogin = () => {
-  ctnLoginForm.classList.remove("d-none");
-  ctnSignupForm.classList.add("d-none");
-};
-let displaySigup = () => {
-  ctnSignupForm.classList.remove("d-none");
-  ctnLoginForm.classList.add("d-none");
-};
+// let btnLogin = document.getElementById("btn-login");
+// let btnSignup = document.getElementById("btn-singup");
+// let btnLogout = document.getElementById("btn-logout");
+// let username = document.getElementById("username");
+// // toàn bộ div chưa form login
+// let ctnLoginForm = document.getElementById("ctn-loginForm");
+// // Toàn bộ điv chứa form signup
+// let ctnSignupForm = document.getElementById("ctn-signupForm");
+// // form login
+// let formSignup = document.getElementById("form-signup");
+// // event khi click click nut dang nhap
+// let displayLogin = () => {
+//   ctnLoginForm.classList.remove("d-none");
+//   ctnSignupForm.classList.add("d-none");
+// };
+// let displaySigup = () => {
+//   ctnSignupForm.classList.remove("d-none");
+//   ctnLoginForm.classList.add("d-none");
+// };
 
-let signupValidate = (e) => {
-  e.preventDefault();
-  let flag = true;
-  let usernameContent = document.getElementById("user");
-  let emailContent = document.getElementById("email");
-  let passwordContent = document.getElementById("password");
-  let passwordConfirmContent = document.getElementById("password-confirm");
-  let messengerUser = document.getElementById("user-invalid");
-  let messengerEmail = document.getElementById("email-invalid");
-  let messengerPassword = document.getElementById("password-invalid");
-  let messengerPasswordConfirm = document.getElementById("passwordConfirm-invalid");
+// let signupValidate = (e) => {
+//   e.preventDefault();
+//   let flag = true;
+//   let usernameContent = document.getElementById("user");
+//   let emailContent = document.getElementById("email");
+//   let passwordContent = document.getElementById("password");
+//   let passwordConfirmContent = document.getElementById("password-confirm");
+//   let messengerUser = document.getElementById("user-invalid");
+//   let messengerEmail = document.getElementById("email-invalid");
+//   let messengerPassword = document.getElementById("password-invalid");
+//   let messengerPasswordConfirm = document.getElementById("passwordConfirm-invalid");
 
-  if (usernameContent.value == "") {
-    usernameContent.classList.add("is-invalid");
-    usernameContent.classList.remove("is-valid");
-    messengerUser.innerHTML = `Vui lòng không để trống`;
-    flag = false;
-  } else {
-    usernameContent.classList.remove("is-invalid");
-    usernameContent.classList.add("is-valid");
-    flag = true;
-  }
+//   if (usernameContent.value == "") {
+//     usernameContent.classList.add("is-invalid");
+//     usernameContent.classList.remove("is-valid");
+//     messengerUser.innerHTML = `Vui lòng không để trống`;
+//     flag = false;
+//   } else {
+//     usernameContent.classList.remove("is-invalid");
+//     usernameContent.classList.add("is-valid");
+//     flag = true;
+//   }
 
-  if (emailContent.value == "") {
-    emailContent.classList.add("is-invalid");
-    emailContent.classList.remove("is-valid");
-    messengerEmail.innerHTML = `Vui lòng nhập email`;
-    flag = false;
-  } else {
-    emailContent.classList.remove("is-invalid");
-    emailContent.classList.add("is-valid");
-    flag = true;
-  }
+//   if (emailContent.value == "") {
+//     emailContent.classList.add("is-invalid");
+//     emailContent.classList.remove("is-valid");
+//     messengerEmail.innerHTML = `Vui lòng nhập email`;
+//     flag = false;
+//   } else {
+//     emailContent.classList.remove("is-invalid");
+//     emailContent.classList.add("is-valid");
+//     flag = true;
+//   }
 
-  if (passwordContent.value == "") {
-    passwordContent.classList.add("is-invalid");
-    passwordContent.classList.remove("is-valid");
-    messengerPassword.innerHTML = `Vui lòng nhập mật khẩu`;
-    flag = false;
-  } else {
-    passwordContent.classList.remove("is-invalid");
-    passwordContent.classList.add("is-valid");
-    flag = true;
-  }
+//   if (passwordContent.value == "") {
+//     passwordContent.classList.add("is-invalid");
+//     passwordContent.classList.remove("is-valid");
+//     messengerPassword.innerHTML = `Vui lòng nhập mật khẩu`;
+//     flag = false;
+//   } else {
+//     passwordContent.classList.remove("is-invalid");
+//     passwordContent.classList.add("is-valid");
+//     flag = true;
+//   }
 
-  if (passwordConfirmContent.value == "" || passwordConfirmContent.value != passwordContent.value) {
-    passwordConfirmContent.classList.add("is-invalid");
-    passwordConfirmContent.classList.remove("is-valid");
-    messengerPasswordConfirm.innerHTML = `Xác nhận mật khẩu không đúng`;
-    flag = false;
-  } else {
-    passwordConfirmContent.classList.add("is-valid");
-    passwordConfirmContent.classList.remove("is-invalid");
-    flag = true 
-  }
+//   if (passwordConfirmContent.value == "" || passwordConfirmContent.value != passwordContent.value) {
+//     passwordConfirmContent.classList.add("is-invalid");
+//     passwordConfirmContent.classList.remove("is-valid");
+//     messengerPasswordConfirm.innerHTML = `Xác nhận mật khẩu không đúng`;
+//     flag = false;
+//   } else {
+//     passwordConfirmContent.classList.add("is-valid");
+//     passwordConfirmContent.classList.remove("is-invalid");
+//     flag = true 
+//   }
 
-};
+// };
 
-let cancelForm = (e) => {
-    e.preventDefault();
-    ctnSignupForm.classList.add("d-none");
-}
-btnLogin.addEventListener("click", displayLogin);
-btnSignup.addEventListener("click", displaySigup);
-formSignup.addEventListener("submit", signupValidate);
-formSignup.addEventListener("reset", cancelForm);
+// let cancelForm = (e) => {
+//     e.preventDefault();
+//     ctnSignupForm.classList.add("d-none");
+// }
+// btnLogin.addEventListener("click", displayLogin);
+// btnSignup.addEventListener("click", displaySigup);
+// formSignup.addEventListener("submit", signupValidate);
+// formSignup.addEventListener("reset", cancelForm);
+
+
+$(".banner-img").slick({
+  arrows: false,
+  fade: true,
+  autoplay: true,
+})
+
+$("#prev").click(function() {
+  $(".banner-img").slick("slickPrev")
+})
+$("#next").click(function() {
+  $(".banner-img").slick("slickNext")
+})
